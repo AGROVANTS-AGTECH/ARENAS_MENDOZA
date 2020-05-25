bbb600c529afb380a22def6c2b92cbac4b660875
AGROVANTS <55609306+AGROVANTS-AGTECH@users.noreply.github.com>
2020-05-25 06:27:00 -0300

    Add files via upload

---
+++ data/CANTERAARENA_MZACANTERAARENA_MZAkmz_5.js
@@ -0,0 +1,8 @@
+var json_CANTERAARENA_MZACANTERAARENA_MZAkmz_5 = {
+"type": "FeatureCollection",
+"name": "CANTERAARENA_MZACANTERAARENA_MZAkmz_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "CANTERA ARENA_MZA", "description": null, "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": 1.0, "extrude": 0.0, "visibility": -1.0, "drawOrder": null, "icon": null }, "geometry": { "type": "Polygon", "coordinates": [ [ [ -68.50258947729381, -37.45031351958901 ], [ -68.496894532380168, -37.453972389732712 ], [ -68.437748364877834, -37.434296142726332 ], [ -68.417288783886519, -37.404780730600002 ], [ -68.415347621853243, -37.385951011633253 ], [ -68.504150462617702, -37.386940033466331 ], [ -68.50258947729381, -37.45031351958901 ] ] ] } }
+]
+}
---
+++ data/CANTERA_MZA_LIBRECANTERA_MZA_LIBREkmz_6.js
@@ -0,0 +1,8 @@
+var json_CANTERA_MZA_LIBRECANTERA_MZA_LIBREkmz_6 = {
+"type": "FeatureCollection",
+"name": "CANTERA_MZA_LIBRECANTERA_MZA_LIBREkmz_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "CANTERA_MZA_LIBRE", "description": null, "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": 1.0, "extrude": 0.0, "visibility": -1.0, "drawOrder": null, "icon": null }, "geometry": { "type": "Polygon", "coordinates": [ [ [ -68.50385043184302, -37.429756096437039 ], [ -68.502485216311683, -37.450278302402452 ], [ -68.496869738970148, -37.453935810780692 ], [ -68.465107978429685, -37.443342785155203 ], [ -68.465179816296853, -37.42965183247918 ], [ -68.50385043184302, -37.429756096437039 ] ] ] } }
+]
+}
